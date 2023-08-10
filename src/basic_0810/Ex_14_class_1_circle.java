633f68f3ad87a8673fad97e3b6c9341d3a5346ec
oyunojino <dusrn2xlayjoh@gmail.com>
2023-08-10 16:40:53 +0900

    클래스 캡슐화 관련 예제

---
+++ src/basic_0810/Ex_14_class_1_circle.java
@@ -0,0 +1,21 @@
+package basic_0810;
+
+public class Ex_14_class_1_circle {
+  public static void main(String[] args) {
+    Circle circle = new Circle();
+    circle.setRadius(10.05);
+    System.out.println("circle = " + circle.getRadius());
+  }
+}
+
+class Circle {
+  private double radius = 0.0;
+
+  public double getRadius() {
+    return radius;
+  }
+
+  public void setRadius(double radius) {
+    this.radius = radius;
+  }
+}
---
+++ src/basic_0810/Ex_15_class_2_useCircle.java
@@ -0,0 +1,9 @@
+package basic_0810;
+
+public class Ex_15_class_2_useCircle {
+  public static void main(String[] args) {
+    Circle c = new Circle();
+    c.setRadius(20.0);
+    System.out.println("c.getRadius() = " + c.getRadius());
+  }
+}
