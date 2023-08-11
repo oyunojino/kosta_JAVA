be984ae74ac6c7fb52a32e3f15ad13b0ad0a4fdc
oyunojino <dusrn2xlayjoh@gmail.com>
2023-08-11 14:22:19 +0900

    triangle(ver.1) 예제

---
+++ src/basic_0811/triangle/Triangle.java
@@ -0,0 +1,45 @@
+package basic_0811.triangle;
+
+/**
+ * Q: 삼각형을 나타내는 Triangle 클래스를 작성
+ * 속성 -> 밑변(실수), 높이(실수)
+ * 동작 -> 넓이 구하기, 접근자, 생성자
+ */
+public class Triangle {
+  private double width = 0.0;
+  private double height = 0.0;
+
+  // 생성자
+  public Triangle(double width, double height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  public double getWidth() {
+    return width;
+  }
+
+  public double getHeight() {
+    return height;
+  }
+
+  public void setWidth(double width) {
+    if (width <= 0.0) {
+      System.out.println("삼각형의 밑변은 0보다 커야 합니다.");
+      return;
+    }
+    this.width = width;
+  }
+
+  public void setHeight(double height) {
+    if (height <= 0.0) {
+      System.out.println("삼각형의 높이는 0보다 커야 합니다.");
+      return;
+    }
+    this.height = height;
+  }
+
+  public double findArea() {
+    return width * height / 2;
+  }
+}
---
+++ src/basic_0811/triangle/TriangleTest.java
@@ -0,0 +1,8 @@
+package basic_0811.triangle;
+
+public class TriangleTest {
+  public static void main(String[] args) {
+    Triangle t = new Triangle(10.0, 5.0);
+    System.out.println("삼각형의 넓이는 " + t.findArea() + "입니다.");
+  }
+}
