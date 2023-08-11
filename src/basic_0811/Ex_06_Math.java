f33e903696e2ade1f7a11d218f21d586eb66d42b
oyunojino <dusrn2xlayjoh@gmail.com>
2023-08-11 11:41:34 +0900

    클래스 변수 설명

---
+++ src/basic_0811/Ex_06_Math.java
@@ -0,0 +1,10 @@
+package basic_0811;
+
+public class Ex_06_Math {
+  public static void main(String[] args) {
+//    # 아래와 같이 작성하지 않더라도 Math 사용할 수 있는 이유
+//      => 클래스 변수이기 때문에
+//    Math math = new Math();
+    System.out.println(Math.PI);
+  }
+}
