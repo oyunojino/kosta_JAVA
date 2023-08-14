091e9f30da81969658404eb7e5087c45b644cea0
oyunojino <dusrn2xlayjoh@gmail.com>
2023-08-14 14:30:02 +0900

    다차원 배열 예제

---
+++ src/basic_0814/Ex_10_Array_6_multi.java
@@ -0,0 +1,26 @@
+package basic_0814;
+
+import java.util.Arrays;
+
+public class Ex_10_Array_6_multi {
+  public static void main(String[] args) {
+    int scores[][] = new int[3][5];
+
+    scores = new int[][]{
+        {100, 90, 80, 70, 60},
+        {100, 70, 80, 90, 100},
+        {90, 90, 90, 90, 90}
+    };
+    int sum[] = new int[3];
+    int total = 0;
+
+    for (int i = 0; i < scores.length; i++) {
+      for (int j = 0; j < scores[i].length; j++) {
+        sum[i] += scores[i][j];
+      }
+      total += sum[i];
+    }
+    System.out.println(Arrays.toString(sum));
+    System.out.println(total);
+  }
+}
