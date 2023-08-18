cf91a902e184ff98a2c44e85348d9d2ca09275ad
KOSTA <dusrn2xlayjoh@gmail.com>
2023-08-18 10:43:24 +0900

    Printer 관련 예제

---
+++ src/basic_0818/Printer.java
@@ -0,0 +1,19 @@
+package basic_0818;
+
+public class Printer {
+    static void println(int num) {
+        System.out.println(num);
+    }
+
+    static void println(boolean num) {
+        System.out.println(num);
+    }
+
+    static void println(double num) {
+        System.out.println(num);
+    }
+
+    static void println(String num) {
+        System.out.println(num);
+    }
+}
---
+++ src/basic_0818/PrinterTest.java
@@ -0,0 +1,21 @@
+package basic_0818;
+
+public class PrinterTest {
+    public static void main(String[] args) {
+
+//       # 객체를 생성o println() 메소드 호출
+//        Printer printer = new Printer();
+//        printer.println(10);
+//        printer.println(true);
+//        printer.println(5.7);
+//        printer.println("홍길동");
+
+
+
+//       # 객체를 생성x println() 메소드 호출
+        Printer.println(10);
+        Printer.println(true);
+        Printer.println(5.7);
+        Printer.println("홍길동");
+    }
+}
