3aad2c755f14fb84d9169ed0791f6619bf37a303
KOSTA <dusrn2xlayjoh@gmail.com>
2023-08-18 11:59:17 +0900

    예시 추가

--- src/basic_0818/MemberTest.java
+++ src/basic_0818/MemberTest.java
@@ -2,10 +2,11 @@ package basic_0818;
 
 public class MemberTest {
     public static void main(String[] args) {
-        Member user1 = new Member("홍길동", "hong");
-        System.out.println("name = " + user1.name + ", id = " + user1.id);
-
         MemberService memberService = new MemberService();
+        Member user1 = new Member("홍길동", "hong");
+        Member m1 = new Member("홍길동", "hong", "12345", 20);
+        Member m2 = new Member("김연아", "kim", "qwert", 20);
+
         boolean result = memberService.login("hong", "12345");
         if(result) {
             System.out.println("로그인 되었습니다.");
@@ -14,5 +15,6 @@ public class MemberTest {
             System.out.println("id 혹은 password가 올바르지 않습니다.");
         }
         result = memberService.login("error", "12345");
+
     }
 }
