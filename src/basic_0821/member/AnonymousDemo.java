a990e647ad67fcc4390c125290af40bfd85e1353
KOSTA <dusrn2xlayjoh@gmail.com>
2023-08-21 15:14:50 +0900

    익명 클래스 관련 예제

--- src/basic_0821/member/AnonymousDemo.java
+++ src/basic_0821/member/AnonymousDemo.java
@@ -5,26 +5,31 @@ public class AnonymousDemo {
 //        # 익명클래스(Anonymous class)
 //        클래스의 선언과 객체의 생성을 동시에 하는 이름 없는 클래스(일회용)
         Parent P = new Parent() {
-            @Override
-            public void parent() {
-                System.out.println("익명클래스에서 구현한 메서드");
-            }
+//            ## Parent가 인터페이스 일 때,
+//            @Override
+//            public void parent() {
+//                System.out.println("익명클래스에서 구현한 메서드");
+//            }
         };
 
 
     }
 }
 
+// ## Parent가 인터페이스 일 때,
 //class OnlyOnce implements Parent {
 //    @Override
 //    public void parent() {
 //        System.out.println("parent");
 //    }
 //}
-
+// ## Parent가 인터페이스 일 때,
 //interface Parent {
 //    void parent();
 //}
+
+
+// ### Parent가 클래스 일 때,
 class Parent {
     void parent() {
         System.out.println("parent");
