682b1a80bd56b4f4b5458947292a572fda8743bd
KOSTA <dusrn2xlayjoh@gmail.com>
2023-08-21 17:34:17 +0900

    Person을 나이를 기준으로 sort 1. compareTo의 리턴값 변형-> person.age - this.age 2. Arrays.sort 후 순서를 뒤집는 메서드 활용 -> Arrays.sort(persons, Collections.reverseOrder());

--- src/basic_0821/person/PersonCompareTest.java
+++ src/basic_0821/person/PersonCompareTest.java
@@ -1,22 +1,38 @@
 package basic_0821.person;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class PersonCompareTest {
     public static void main(String[] args) {
         Person[] persons = {
-                new Person("홍길동",32, 170, 75),
-                new Person("박지성",40, 175, 68),
-                new Person("손흥민",20, 180, 70),
+                new Person("홍길동", 32, 170, 75),
+                new Person("박지성", 40, 175, 68),
+                new Person("손흥민", 20, 180, 70),
         };
+
+        System.out.println("정렬 전");
+        for (Person person : persons) {
+            System.out.println(person);
+        }
+
+//        Arrays.sort(persons);
+        Arrays.sort(persons, Collections.reverseOrder());
+
+        System.out.println("정렬 후");
+        for (Person person : persons) {
+            System.out.println(person);
+        }
 
 //        Q: 나이를 기준으로 나이가 가장 많은 사람부터 이름을 출력하시오.
 //          -> 두가지 방법을 이용
-//          1. compareTo의 리턴값 변형
+//          1. compareTo의 리턴값 변형( person.age - this.age )
 //          2. Arrays.sort 후 순서를 뒤집는 메서드 활용
 
     }
 }
 
-class Person implements Comparable<Person>{
+class Person implements Comparable<Person> {
     String name;
     int age;
     int height;
@@ -31,9 +47,18 @@ class Person implements Comparable<Person>{
 
     @Override
     public int compareTo(Person person) {
-        
-        return 0;
+//        return person.age - this.age;
+        return this.age - person.age;
     }
 
-//    메서드
+    //    메서드
+    @Override
+    public String toString() {
+        return "사람[" +
+                "이름='" + name + '\'' +
+                ", 나이=" + age +
+                ", 키=" + height +
+                ", 몸무게=" + weight +
+                ']';
+    }
 }
