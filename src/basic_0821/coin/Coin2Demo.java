440cfe1bbc6b1ad2454785e0a72dc65e61d21fac
KOSTA <dusrn2xlayjoh@gmail.com>
2023-08-21 10:38:51 +0900

    인터페이스 상속 예제

---
+++ src/basic_0821/coin/Coin2Demo.java
@@ -0,0 +1,9 @@
+package basic_0821.coin;
+
+import basic_0821.coin.Coin;
+
+public class Coin2Demo implements Coin {
+    public static void main(String[] args) {
+        System.out.println("DIME은 " + DIME + " 센트입니다.");
+    }
+}
