c349566ccc5518967cabb2218d20d706bb64c375
KOSTA <dusrn2xlayjoh@gmail.com>
2023-08-22 11:07:14 +0900

    프로그래밍 문제 6 주석 추가 -> 07.인터페이스와 특수클래스

--- src/basic_0822/HumanTest.java
+++ src/basic_0822/HumanTest.java
@@ -4,7 +4,7 @@ public class HumanTest {
     public static void main(String[] args) {
         Human.echo();
 
-        Student s = new Student();
+        Student s = new Student(20);
         s.print();
         s.eat();
 
@@ -15,12 +15,18 @@ public class HumanTest {
 }
 
 interface Human {
+//    # @Override 하지 않기 위해서 static을 붙여줌
     static void echo() {
         System.out.println("야호!!!");
     }
 
     void eat();
-    void print();
+
+//    # interface에 추가 할 때, 오류 안 나도록 넣는 방법
+    default void print() {
+//        기본값을 지정
+        System.out.println("인간입니다.");
+    }
 }
 
 class Worker implements Human {
@@ -28,20 +34,23 @@ class Worker implements Human {
     public void eat() {
         System.out.println("빵을 먹습니다.");
     }
-
-    @Override
-    public void print() {
-        System.out.println("인간입니다.");
-    }
 }
 
 class Student implements Human {
+    int age;
+
+    public Student(int age) {
+        this.age = age;
+    }
+
     @Override
     public void eat() {
         System.out.println("도시락을 먹습니다.");
     }
+
     @Override
     public void print() {
-        System.out.println("20세의 학생입니다.");
+        System.out.println(age + "세의 학생입니다.");
     }
+    
 }
