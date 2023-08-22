05e7a61f21bf8dad0d74298392d33212d49f8d4f
KOSTA <dusrn2xlayjoh@gmail.com>
2023-08-22 14:24:54 +0900

    .

--- src/basic_0822/StringBuilderDemo.java
+++ src/basic_0822/StringBuilderDemo.java
@@ -14,5 +14,18 @@ public class StringBuilderDemo {
         System.out.println(sb.hashCode()); // hi -> 주소값 100
         sb = sb.append("!");
         System.out.println(sb.hashCode()); // hi! -> 주소값 100
+
+        System.out.println("---------------------------------");
+
+        System.out.println("문자열의 길이 : " + sb.capacity());
+        sb = sb.delete(7, 12);
+        System.out.println("world 삭제 후 " + sb);
+        sb = sb.insert(7, "world");
+        System.out.println("world를 7번째 자리에 추가 후 : " + sb);
+        sb = sb.replace(7, 12, "JAVA");
+        System.out.println("world를 JAVA로 변경 후 : " + sb);
+
+        sb = sb.replace(0, 6, "HELLO,").reverse();
+        System.out.println("hello를 HELLO로 변경 후 리버스 : " + sb);
     }
 }
