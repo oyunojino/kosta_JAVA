abd9c9af71bb0164e0963aab6e2afefc09b38802
KOSTA <dusrn2xlayjoh@gmail.com>
2023-08-23 10:35:50 +0900

    java.util.Date 실습

--- src/basic_0822/SystemDemo.java
+++ src/basic_0822/SystemDemo.java
@@ -1,13 +1,21 @@
 package basic_0822;
 
-public class SystemDemo {
-    public static void main(String[] args) {
-        int[] numbers = new int[1000];
-        System.out.println(System.nanoTime());
+import java.util.Date;
 
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = i+1;
-        }
-        System.out.println(System.nanoTime());
+public class SystemDemo {
+  public static void main(String[] args) {
+    int[] numbers = new int[1000];
+    System.out.println(System.nanoTime());
+
+    for (int i = 0; i < numbers.length; i++) {
+      numbers[i] = i + 1;
     }
+    System.out.println(System.nanoTime());
+
+    System.out.println("---------------------------");
+//        기준 ) 1970년 1월 1일 0시
+    System.out.println(System.currentTimeMillis());
+//        long 타입의 숫자를 날짜로 변환
+    System.out.println(new Date(1692751568736l));
+  }
 }
