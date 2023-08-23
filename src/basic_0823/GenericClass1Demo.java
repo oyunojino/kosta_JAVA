605be8060d34ec68e860a698a5a0087d4e1a80a8
KOSTA <dusrn2xlayjoh@gmail.com>
2023-08-23 17:39:55 +0900

    제네릭 형식으로 변경

--- src/basic_0823/GenericClass1Demo.java
+++ src/basic_0823/GenericClass1Demo.java
@@ -4,24 +4,37 @@ import java.util.ArrayList;
 
 public class GenericClass1Demo {
   public static void main(String[] args) {
+    ArrayList<Boricha> arrayListBoricha = new ArrayList<>();
+    arrayListBoricha.add(new Boricha());
+    //arrayListBoricha.add(new Beer());
+    ArrayList<Beer> arrayListBeer = new ArrayList<>();
+    arrayListBeer.add(new Beer());
+    //arrayListBeer.add(new Boricha())
+
     // get(데이터형 확인 O), set(데이터형 확인 X)
     Cup c = new Cup();
     c.setBeverage(new Boricha());
-    if (c.getBeverage() instanceof Boricha) {
+    if(c.getBeverage() instanceof Boricha){
       Boricha boricha = (Boricha) c.getBeverage();
-    } else if (c.getBeverage() instanceof Beer) {
-      Beer beer = (Beer) c.getBeverage();
+    } else if (c.getBeverage() instanceof Beer){
+      Beer beer= (Beer) c.getBeverage();
     }
 
     c.setBeverage(new Beer());
+    Beer beer= (Beer) c.getBeverage();
     c.setBeverage(new Beverage());
     c.setBeverage(new Object());
+    //beer= (Beer) c.getBeverage();
 
-  // set(데이터형 확인 O) 컴파일 에러가 나도록 유도함
-    ArrayList<Boricha> arrayListBoricha = new ArrayList<>();
-    arrayListBoricha.add(new Boricha());
-//    arrayListBoricha.add(new Beer());
+    // set(데이터형 확인 O) 컴파일 에러가 나도록 유도함
+    Cup<Boricha> borichaCup = new Cup();
+    borichaCup.setBeverage(new Boricha());
+    Boricha boricha = borichaCup.getBeverage();
 
+    Cup<Beer> beerCup = new Cup<>();
+    beerCup.setBeverage(new Beer());
+    //beerCup.setBeverage(new Boricha());
+    beer = beerCup.getBeverage();
   }
 }
 
@@ -37,14 +50,14 @@ class Beer extends Beverage {
 
 }
 
-class Cup {
-  private Object beverage;
+class Cup<T> {
+  private T beverage;
 
-  public Object getBeverage() {
+  public T getBeverage() {
     return beverage;
   }
 
-  public void setBeverage(Object beverage) {
+  public void setBeverage(T beverage) {
     this.beverage = beverage;
   }
 }
