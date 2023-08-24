6544a86a44aaeb6055ff80c828cd2b6b428bcd32
KOSTA <dusrn2xlayjoh@gmail.com>
2023-08-24 15:22:14 +0900

    Collection(List) 내용 예제

--- src/basic_0824/PerformanceDemo.java
+++ src/basic_0824/PerformanceDemo.java
@@ -8,6 +8,7 @@ public class PerformanceDemo {
     ArrayList<Integer> arrayList = new ArrayList<>();
     LinkedList<Integer> linkedList = new LinkedList<>();
 
+//    # ArrayList 추가 -> 뒤
     long start = System.nanoTime();
     for (int i = 0; i < 100_000; i++) {
       arrayList.add(i);
@@ -16,8 +17,46 @@ public class PerformanceDemo {
     long arrayListTime = end - start;
     System.out.println("arrayList 뒤에 값을 추가할 때, " + arrayListTime);
 
+
     System.out.println("-------------------------------");
 
+//    # ArrayList 추가 -> 앞
+    start = System.nanoTime();
+    for (int i = 0; i < 100_000; i++) {
+      arrayList.add(0, i);
+    }
+    end = System.nanoTime();
+    arrayListTime = end - start;
+    System.out.println("arrayList 앞에 값을 추가할 때, " + arrayListTime);
+
+
+    System.out.println("-------------------------------");
+
+//    # ArrayList 삭제 -> 뒤
+    start = System.nanoTime();
+    for (int i = 0; i < 100_000; i++) {
+      arrayList.remove(arrayList.size() - 1);
+    }
+    end = System.nanoTime();
+    arrayListTime = end - start;
+    System.out.println("arrayList 뒤에 값을 삭제할 때, " + arrayListTime);
+
+
+    System.out.println("-------------------------------");
+
+//    # ArrayList 삭제 -> 앞
+    start = System.nanoTime();
+    for (int i = 0; i < 100_000; i++) {
+      arrayList.remove(0);
+    }
+    end = System.nanoTime();
+    arrayListTime = end - start;
+    System.out.println("arrayList 앞에 값을 삭제할 때, " + arrayListTime);
+
+
+    System.out.println("-------------------------------");
+
+//    # LinkedList 추가 -> 뒤
     start = System.nanoTime();
     for (int i = 0; i < 100_000; i++) {
       linkedList.add(i);
@@ -26,18 +65,10 @@ public class PerformanceDemo {
     long linkedListTime = end - start;
     System.out.println("linkedList 뒤에 값을 추가할 때, " + linkedListTime);
 
-    System.out.println("-------------------------------");
-
-    start = System.nanoTime();
-    for (int i = 0; i < 100_000; i++) {
-      arrayList.add(0,i);
-    }
-    end = System.nanoTime();
-    arrayListTime = end - start;
-    System.out.println("arrayList 앞에 값을 추가할 때, " + arrayListTime);
 
     System.out.println("-------------------------------");
 
+//    # LinkedList 추가 -> 앞
     start = System.nanoTime();
     for (int i = 0; i < 100_000; i++) {
       linkedList.add(0, i);
@@ -45,5 +76,35 @@ public class PerformanceDemo {
     end = System.nanoTime();
     linkedListTime = end - start;
     System.out.println("linkedList 앞에 값을 추가할 때, " + linkedListTime);
+
+
+    System.out.println("-------------------------------");
+
+//    # LinkedList 삭제 -> 뒤
+    for (int i = 0; i < 100_000; i++) {
+      linkedList.add(0, i);
+    }
+
+    start = System.nanoTime();
+    for (int i = 0; i < 100_000; i++) {
+      linkedList.removeLast();
+    }
+    end = System.nanoTime();
+    linkedListTime = end - start;
+    System.out.println("linkedList 뒤에서 값을 삭제할 때, " + linkedListTime);
+
+
+    System.out.println("-------------------------------");
+
+//    # LinkedList 삭제 -> 앞
+    start = System.nanoTime();
+    for (int i = 0; i < 100_000; i++) {
+      linkedList.removeFirst();
+    }
+    end = System.nanoTime();
+    linkedListTime = end - start;
+    System.out.println("linkedList 앞에서 값을 삭제할 때, " + linkedListTime);
+
+
   }
 }
