b8ab9447e2e02b8575160640bf7f4109ce5ece23
KOSTA <dusrn2xlayjoh@gmail.com>
2023-09-01 17:33:38 +0900

    멀티 스레드 예제2

---
+++ src/basic_0901/ThreadDemo2.java
@@ -0,0 +1,37 @@
+package basic_0901;
+
+public class ThreadDemo2 {
+  public static void main(String[] args) {
+    //Thread thread =
+//    new Thread(new Runnable() {
+//      @Override
+//      public void run() {
+//        for (int i = 0; i < 5 ; i++) {
+//          System.out.println("안녕!");
+//          try {
+//            Thread.sleep(500);
+//          } catch (InterruptedException e) {
+//          }
+//        }
+//      }
+//    }).start();
+
+    new Thread(() -> {
+      for (int i = 0; i < 5; i++) {
+        System.out.println("안녕!");
+        try {
+          Thread.sleep(500);
+        } catch (InterruptedException e) {
+        }
+      }
+    }).start();
+
+    for (int i = 0; i < 5 ; i++) {
+      System.out.println("메인입니다.");
+      try {
+        Thread.sleep(500);
+      } catch (InterruptedException e) {
+      }
+    }
+  }
+}
